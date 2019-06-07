248b728dc9cd0e202de6ddf47d031819425443c4
gautierrognon <gautier.rognon@coopengo.com>
2019-06-07 09:30:30 +0200

    Adapt docker file for every version

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,18 @@
 # syntax=docker/dockerfile:experimental
 
-FROM coopengo/coog:master-latest
+ARG IMPORT
+
+FROM $IMPORT
 
 ARG CUSTOMER
 ARG VERSION_TAG
-
-# Install ssh client and git
-RUN apk add --no-cache openssh-client git
+ARG VERSION
 
 # Download public key for github.com
 RUN mkdir -p -m 0600 ~/.ssh && ssh-keyscan github.com >> ~/.ssh/known_hosts
 
 # Clone private repository
-RUN --mount=type=ssh git clone --single-branch --branch $CUSTOMER-coog-2.0 git@github.com:coopengo/customers.git
+RUN --mount=type=ssh git clone --single-branch --branch $CUSTOMER-coog-$VERSION git@github.com:coopengo/customers.git
 
 #RUN git -b checkout $VERSION_TAG
 
---
+++ automate_build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+USAGE="""
+Usage: ./report VERSION
+	VERSION: the version you want to test on for every client
+	You must precise only one version to test
+	ex : 2.0.30
+  """
+echo "$USAGE"
+
+#if there is no input or too many inputs
+[ $# -ne 1 ] && echo "$Usage" && exit 1
+
+#if variable is empty
+[ -z "$1" ] && echo "Missing version number. $USAGE" && exit 1
+
+major_version="$(cut -d'.' -f1 <<<"$1")"
+minor_version="$(cut -d'.' -f2 <<<"$1")" 
+version="$major_version.$minor_version"
+
+docker_import="coopengo/coog:coog-$version"
+
+tag_version=$1
+
+file="customers"
+
+while IFS= read -r line
+do
+    NAME=${line%:*}
+	docker build --tag="coopengo/coog-$NAME:$tag_version" --build-arg CUSTOMER="$NAME" --build-arg VERSION_TAG="$tag_version" --build-arg IMPORT="$docker_import" --build-arg VERSION="$version" --ssh default .
+
+done < "$file"
