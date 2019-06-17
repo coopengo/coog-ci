12805f06cfac206fc5c023d0b1da240c0e08ecb2
gautierrognon <gautier.rognon@coopengo.com>
2019-06-17 14:08:59 +0200

    intergrate docker image build in report.sh

--- build_coog_customers.sh
+++ build_coog_customers.sh
@@ -1,47 +1,81 @@
 #!/bin/bash
 
 USAGE="""
-Usage: ./build_coog_customers.sh VERSION CLIENT
-	VERSION: the version you want to build
-	You must precise only one version to build
+Usage: ./build_coog_customers.sh -v TAG_VERSION -c CUSTOMER
 
-	CLIENT: "--all" if you want to build image for each client in the customers file
-			you can build only one client image by replacing --all by the client name
+	-v TAG_VERSION: The tag version you release.
+
+	-c CUSTOMER: the customer you release version for.
+	             use "--all" if you want to build image for every customers in "customers" file
 
 	ex: ./build_coog_customers.sh 2.0.30 --all
   """
 
-#if there is no input
-[ $# -ne 2 ] && echo "$USAGE" && exit 1
+_get_version_from_tag(){
+	major_version="$(cut -d'.' -f1 <<<"$1")"
+	minor_version="$(cut -d'.' -f2 <<<"$1")"
+	version="$major_version.$minor_version"
+}
+
+_get_docker_import(){
+	DOCKER_IMPORT="coopengo/coog:$1"
+}
+
+_build_image_customer(){
+	if [ $1 = "--all" ]
+	then
+		file="customers"
+	    while IFS= read -r line
+	    do
+	    	NAME=${line%:*}
+	    	docker build --tag="coopengo/coog-$NAME:$2" --build-arg CUSTOMER="$NAME" --build-arg VERSION_TAG="$2" --build-arg IMPORT="$DOCKER_IMPORT" --build-arg VERSION="$version" --ssh default .
+	    done < "$file"
+	else
+		echo "$1"
+		NAME="$1"
+		docker build --tag="coopengo/coog-$NAME:$2" --build-arg CUSTOMER="$NAME" --build-arg VERSION_TAG="$2" --build-arg IMPORT="$DOCKER_IMPORT" --build-arg VERSION="$version" --ssh default .
+	fi
+}
+
+main() {
+	_get_version_from_tag $TAG_VERSION
+	_get_docker_import $TAG_VERSION
+	_build_image_customer $CUSTOMER $TAG_VERSION
+}
+
+
+
+#Script starts here
+[ $# -lt 4 ] && echo "$USAGE" && exit 1
+
+while [[ $# -gt 1 ]]; do
+
+	arg="$1"
+	value="$2"
+	case $arg in
+		-c|--customer)
+            CUSTOMER="$value"
+            shift
+            ;;
+        -v|--version)
+            TAG_VERSION="$value"
+            shift
+            ;;
+        *)
+            echo "Invalid argument $arg."
+            return 1
+            ;;
+    esac
+    shift
+done
 
 #if one of the variable is empty
-[ -z "$1" ] && echo "Missing version number. $USAGE" && exit 1
-[ -z "$2" ] && echo "Missing customers name. $USAGE" && exit 1
+[ -z "$TAG_VERSION" ] && echo "Missing version number. $USAGE" && exit 1
+[ -z "$CUSTOMER" ] && echo "Missing customers name. $USAGE" && exit 1
 
 export DOCKER_BUILDKIT=1
 
-major_version="$(cut -d'.' -f1 <<<"$1")"
-minor_version="$(cut -d'.' -f2 <<<"$1")"
-version="$major_version.$minor_version"
+main "$@"
 
-tag_version=$1
 
-docker_import="coopengo/coog:$tag_version"
 
-echo "$2"
-
-#if we want to build image for every client
-if [ $2 = "--all" ]
-then
-	file="customers"
-	while IFS= read -r line
-	do
-		NAME=${line%:*}
-		docker build --tag="coopengo/coog-$NAME:$tag_version" --build-arg CUSTOMER="$NAME" --build-arg VERSION_TAG="$tag_version" --build-arg IMPORT="$docker_import" --build-arg VERSION="$version" --ssh default .
-	done < "$file"
-
-#if we want to build the image for one specific client
-else 
-	NAME="$2"
-	docker build --tag="coopengo/coog-$NAME:$tag_version" --build-arg CUSTOMER="$NAME" --build-arg VERSION_TAG="$tag_version" --build-arg IMPORT="$docker_import" --build-arg VERSION="$version" --ssh default .
-fi
